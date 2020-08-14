7eaa36cdea52a8d613c71fd5eec16d7f8af1ae61
Luis Silva <65371615+Luis1r@users.noreply.github.com>
2020-08-14 10:43:26 -0500

    Add files via upload
    show login status on the site navigation bar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -19,43 +19,85 @@ class Navbar extends Component {
       : "nav-item";
   };
   render() {
-    return (
-      <nav className="menu navbar navbar-expand navbar-dark">
-        <Link className="navbar-brand" to="/">
-        <img src={logo} alt="Logo" className="menu__logo"/>
-        </Link>
-        <button className="navbar-toggler" type="button"
-          data-toggle="collapse" data-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="menu__right" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className={this.getNavLinkClass("/")} >
-              <Link className="menu__list-item nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className={this.getNavLinkClass("/aboutUs")} >
-              <Link className="menu__list-item nav-link" to="/aboutUs">
-                About
-              </Link>
-            </li>
-            <li className={this.getNavLinkClass("/myUploads")} >
-              <Link className="menu__list-item nav-link" to="/myUploads">
-                Create Account
-              </Link>
-            </li>
-            <li className={this.getNavLinkClass('/myNode' )} >
-              <Link className="menu__list-item nav-link" to="/myNode">
-                Login
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
+    var isLogin = true;
+    if (isLogin){
+      return (
+        <nav className="menu navbar navbar-expand navbar-dark">
+          <Link className="navbar-brand" to="/">
+          <img src={logo} alt="Logo" className="menu__logo"/>
+          </Link>
+          <button className="navbar-toggler" type="button"
+            data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="menu__right" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className={this.getNavLinkClass("/")} >
+                <Link className="menu__list-item nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className={this.getNavLinkClass("/myUploads")} >
+                <Link className="menu__list-item nav-link" to="/myUploads">
+                  My Uploads
+                </Link>
+              </li>
+              <li className={this.getNavLinkClass('/myNode' )} >
+                <Link className="menu__list-item nav-link" to="/myNode">
+                  My node
+                </Link>
+              </li>
+              <li className={this.getNavLinkClass('/aboutUs' )} >
+                <Link className="menu__list-item nav-link" to="/aboutUs">
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      );
+    }
+    else{
+      return (
+        <nav className="menu navbar navbar-expand navbar-dark">
+          <Link className="navbar-brand" to="/">
+          <img src={logo} alt="Logo" className="menu__logo"/>
+          </Link>
+          <button className="navbar-toggler" type="button"
+            data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="menu__right" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className={this.getNavLinkClass("/")} >
+                <Link className="menu__list-item nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className={this.getNavLinkClass("/aboutUs")} >
+                <Link className="menu__list-item nav-link" to="/aboutUs">
+                  About
+                </Link>
+              </li>
+              <li className={this.getNavLinkClass("/myUploads")} >
+                <Link className="menu__list-item nav-link" to="/myUploads">
+                  Create Account
+                </Link>
+              </li>
+              <li className={this.getNavLinkClass('/myNode' )} >
+                <Link className="menu__list-item nav-link" to="/myNode">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      );
+    }
   }
 }
 
